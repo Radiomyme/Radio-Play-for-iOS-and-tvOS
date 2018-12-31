01108cfb8e423181ceff2acbcadfa51c2727aaf4
Radiomyme <radiomyme@live.fr>
2018-12-31 12:20:59 +0100

    Add files via upload

---
+++ RadioPlay for iOS/main.m
@@ -0,0 +1,24 @@
+//
+//  Radio7_Prefix.pch
+//  Radio 7 Pro
+//11
+//  Created by Christopher Coudriet on 10/15/2012.
+//  Copyright Christopher Coudriet 2012. All rights reserved.
+//
+//  Permission is given to license this source code file, in any
+//  project, commercial or otherwise, entirely at your risk, with the condition
+//  that this source code cannot be redistributed or sold (in part or whole) and must retain
+//  this copyright and permission notice. Attribution in compiled projects is
+//  appreciated but not required.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
